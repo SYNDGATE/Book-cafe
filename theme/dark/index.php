ce9638d3c6f498f010baff14b558717368031174
SYNDGATE <syndgate@naver.com>
2024-08-08 18:52:58 +0900

    1차 수정 테스트

--- theme/dark/index.php
+++ theme/dark/index.php
@@ -11,7 +11,7 @@ include_once(G5_THEME_PATH . '/head.php');
 <!-- Container End -->
 <section id="main-top" class="relative w-full flex items-center">
   <div class="container mx-auto sm: px-4 text-white relative" data-aos="zoom-out" data-aos-delay="100" data-aos-duration="1000">
-    <h1 class="m-0 text-xl md:text-3xl font-extrabold leading-6 md:leading-8 ">늘푸른노인복지관 <span class="text-indigo-500"> 도서정보검색</span></h1>
+    <h1 class="m-0 text-xl md:text-3xl font-extrabold leading-6 md:leading-8 "><?php echo $g5['title'] ?> <span class="text-indigo-500"> 도서정보검색</span></h1>
     <h2 class="mt-2 mr-0 mb-10 ml-0 text-lg md:text-xl font-base mb-0 md:mb-4">도서관리는 검색 정보 제공 사이트입니다.</h2>
     <div class="inline-flex items-center">
       <a href="./book" target="_blank" class="uppercase text-base md:text-lg tracking-wide inline-block py-1.5 px-3 rounded-md transition transition-duration-500 bg-indigo-500 hover:bg-indigo-400">시작하기</a>
